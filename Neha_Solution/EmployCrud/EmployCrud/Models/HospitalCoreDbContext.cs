53d3e308e92c8b1b1e696d1e55823445b9510e33
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-03-11 12:37:19 +0530

    code

---
+++ Neha_Solution/EmployCrud/EmployCrud/Models/HospitalCoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployCrud.Models
+{
+    public class HospitalCoreDbContext : DbContext
+    {
+        public HospitalCoreDbContext(DbContextOptions<HospitalCoreDbContext> options) : base(options)
+        {
+        }
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>().ToTable("Doctor");
+        }
+
+
+        public DbSet<Doctor> Doctor { get; set; }
+    }
+}
