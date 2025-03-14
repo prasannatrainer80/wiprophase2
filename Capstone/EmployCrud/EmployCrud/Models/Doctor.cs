eb7bba6f37863401b7d8171a44de1c529998b68c
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-03-14 11:16:06 +0530

    code

---
+++ Capstone/EmployCrud/EmployCrud/Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployCrud.Models
+{
+    public class Doctor
+    {
+        public int DoctorId { get;set; }
+        public string DoctorName { get;set; }   
+        public string Speciality {  get;set; }
+        public string Qualification { get; set; } 
+        public string DoctorUserName { get; set; }
+        public string DoctorPassword { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+    }
+}
