bbb10d4d3199d3a5305939b1881afb9a06d766ed
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-03-14 12:04:01 +0530

    code

---
+++ Capstone/Mailing_Code/EmployCrud/EmployCrud/Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployCrud.Models;
+using EmployCrud.Middleware;
+
+namespace EmployCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly EFCoreDbContext _context;
+
+        public UsersController(EFCoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("login/{email}/{password}")]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            string encr = EncryptionHelper.Encrypt(password);
+            Users user = _context.Users.Where(x => x.Email == email && x.PasswordHash == encr).FirstOrDefault<Users>();
+            //var token = await _authService.Authenticate(request.Username, request.Password);
+            if (user==null)
+            {
+                return Unauthorized("Invalid credentials");
+            }
+            return Ok("Correct ");
+        }
+    
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return users;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsers(int id, Users users)
+        {
+            if (id != users.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Users>> PostUsers(Users users)
+        {
+            SendMail.MailInfo("tuhinpaul4602@gmail.com", ".NET Core Mail", "This is First Mail");
+            string encry = EncryptionHelper.Encrypt(users.PasswordHash);
+            users.PasswordHash = encry;
+            _context.Users.Add(users);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsers", new { id = users.UserId }, users);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsers(int id)
+        {
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(users);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UsersExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ Capstone/Mailing_Code/EmployCrud/EmployCrud/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EmployCrud.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string? FullName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PasswordHash { get; set; }    
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
